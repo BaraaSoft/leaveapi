dfc85b0cd75eedc0ece07a108abccb01c862f639
baraabytes <baraaabuzeid@hotmail.com>
2019-05-10 15:24:58 +0800

    azure changes

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+'use strict';
+var port = process.env.PORT || 3001;
 
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 var express = require('express');
 
 server.use('/hub', express.static("hub"));
-server.use(middlewares)
-server.use('/api', router)
-server.listen(3001, () => {
+server.use(middlewares);
+server.use('/api', router);
+server.listen(port, () => {
     console.log('>> JSON Server is running...');
-    console.log(">> " + server.path())
+    console.log(">> " + server.path());
     //console.log("Path:")
     //console.log(__dirname)
-})
+});
 
-// $ node server.js
+
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
